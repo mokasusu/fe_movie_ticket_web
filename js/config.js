4030ee7c1a826e00a69c3bf6a6b65049b1fd0c68
mokasusu <vietsu210205@gmail.com>
2025-12-21 23:08:03 +0700

    sửa import

--- js/config.js
+++ js/config.js
@@ -1,9 +1,3 @@
-// js/config.js
+const isGitHub = window.location.hostname.includes('github.io');
 
-const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-const repoName = 'cop_cinema'; // Tên chính xác của repo trên GitHub
-
-// Nếu là localhost thì rỗng, nếu là GitHub thì thêm /cop_cinema
-export const BASE_PATH = isLocal ? '' : `/${repoName}`;
-
-console.log('Current BASE_PATH:', BASE_PATH); 
+export const BASE_PATH = isGitHub ? '/cop_cinema' : '';
